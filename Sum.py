237827db81daed9214b90cf78256c638a6e7e799
Yuvraj-Nucleusteq <yuvraj.bhadouria@nucleusteq.com>
2023-03-17 17:16:34 +0530

    latest

--- Sum.py
+++ Sum.py
@@ -1,14 +1,6 @@
 def sum(a,b):
     return a+b
-def subs(a,b):
-    return a-b
-def mul(a,b):
-    return a*b
-def div(a,b):
-    return a/b
 x = 2
 y = 4
 print (sum(int(x),int(y)))
-print (subs(int(x),int(y)))
-print (mul(int(x),int(y)))
-print (div(int(x),int(y)))
+
--- file.py
+++
@@ -1,5 +0,0 @@
-def sum(a,b):
-    return a+b
-x = 2
-y = 4
-print (sum(int(x),int(y)))
