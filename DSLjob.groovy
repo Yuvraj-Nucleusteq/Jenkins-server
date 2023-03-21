fe9e3e989f88ebbcf6fef43038d2c2ab1a4b1aec
Yuvraj-Nucleusteq <yuvraj.bhadouria@nucleusteq.com>
2023-03-21 12:16:54 +0530

    latest

--- DSLjob.groovy
+++ DSLjob.groovy
@@ -1,12 +1,10 @@
-job('DSL-job'){
-    description("running job through job-dsl")
-    scm{
-        git("https://github.com/Yuvraj-Nucleusteq/Jenkins-server.git","main")
-    }
-    triggers{
-        githubPush()
-    }
-    steps{
-        shell("flake8 . --exit-zero")
-    }
+pipeline {
+agent any
+stages {
+stage('Build') {
+steps {
+sh'flake8 . --exit-zero'
+  }
+}
+}
 }
