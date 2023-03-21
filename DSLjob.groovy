157464aaf473eecc6d1c1b0170140a88fe5f78b2
Yuvraj-Nucleusteq <yuvraj.bhadouria@nucleusteq.com>
2023-03-21 12:52:02 +0530

    latest

--- DSLjob.groovy
+++ DSLjob.groovy
@@ -3,11 +3,11 @@ agent any
 stages {
 stage('Build') {
 steps {
-sh'flake8 . --exit-zero --format=pylint'
+sh'flake8 . --exit-zero --format=pylint -o report.txt'
   }
   post{
     always{
-        scanForIssues tool: flake8(pattern: 'Sum.py')
+        scanForIssues tool: flake8(pattern: 'report.txt')
     }
 }
 }
