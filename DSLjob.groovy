e5343e7bd63e0ec70276be171fecae7d32bd959c
Yuvraj-Nucleusteq <yuvraj.bhadouria@nucleusteq.com>
2023-03-21 13:03:52 +0530

    latest

--- DSLjob.groovy
+++ DSLjob.groovy
@@ -3,7 +3,7 @@ agent any
 stages {
 stage('Build') {
 steps {
-sh'flake8 . --exit-zero --format=pylint --output-file=/flake8.log'
+sh'flake8 . --exit-zero --format=pylint --output-file=/var/lib/jenkins/workspace/DSL-test/flake8.log'
   }
   post{
     always{
