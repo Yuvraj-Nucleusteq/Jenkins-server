8be4da58425ff112c46022abef71770a57ae7b07
Yuvraj-Nucleusteq <yuvraj.bhadouria@nucleusteq.com>
2023-03-24 17:09:22 +0530

    latest

--- Sum.py
+++ Sum.py
@@ -1,14 +1,7 @@
-def sum(a,b):
+def sum(a, b):
     return a+b
-def sum(a,b):
-    return a+b
-def sum(a,b):
-    return a+b
-def sum(a,b):
-    return a+b
+
+
 x = 2
 y = 5
-print (sum(int(x),int(y)))
-
-
-
+print(sum(int(x), int(y)))
